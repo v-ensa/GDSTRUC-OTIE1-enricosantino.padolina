452195dab2e0226417ec1b37877266635f3b2ca1
loonathevalley <henrysunnyv@gmail.com>
2026-09-02 10:14:32 +0800

    Implement user input

--- SampleProject/src/Main.java
+++ SampleProject/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Enter Name: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
     }
 }
